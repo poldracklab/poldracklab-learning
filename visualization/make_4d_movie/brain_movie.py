fa5cbb7c6bab49153151f83ebe51fd2f3b1ec210
vsoch <vsochat@stanford.edu>
2015-01-18 23:26:39 -0800

    adding first test of making brain GIF from 4D image, see brain.GIF

---
+++ visualization/make_4d_movie/brain_movie.py
@@ -0,0 +1,39 @@
+# Let's try making a 4d movie from a 4d nifti in python
+# Will be incoporated into pycomparebrain, just learning for now
+
+from nilearn.plotting import plot_stat_map
+import matplotlib.pyplot as plt
+import nibabel as nib
+import contextlib
+import numpy as np
+import tempfile
+import shutil
+import contextlib
+import os, sys
+
+
+# Function to make temporary directory
+@contextlib.contextmanager
+def make_tmp_folder():
+  temp_dir = tempfile.mkdtemp()
+  yield temp_dir
+  shutil.rmtree(temp_dir)
+
+four_dee_file = sys.argv[1]
+gif_name = sys.argv[2]
+header = nib.load(four_dee_file)
+data = header.get_data()
+
+all_images = []
+with make_tmp_folder() as tmp_dir:
+  print "Reading in images to temp..."
+  for tp in range(0,np.shape(data)[3]):
+    timepoint = data[:,:,:,tp]
+    timepoint_img = nib.Nifti1Image(timepoint,affine=header.get_affine())    
+    display = plot_stat_map(timepoint_img)  
+    image_name = '%s/%s_chicken.png' %(tmp_dir,tp)
+    display.savefig(image_name) 
+    plt.close()
+    all_images.append(image_name)  
+    command = " ".join(all_images)
+    os.system("convert -delay 50 %s -loop 1 %s" %(command,gif_name))
---
+++ visualization/make_4d_movie/make_brain_movie.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+# Example of making 4d brain image - buggy for now, but does the trick!
+# Make sure you let it finish run!!
+python brain_movie.py priors_in_BRAINMETA.nii.gz brain.GIF
