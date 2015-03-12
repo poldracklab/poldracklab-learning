cd754130a087994f509f2be33905fb744b21b944
vsoch <vsochat@stanford.edu>
2015-03-12 10:50:45 -0700

    updating timeseries extraction scripts - these are the ones on sherlock

--- databases/hcp/extract_petersen_timeseries.py
+++ databases/hcp/extract_petersen_timeseries.py
@@ -1,48 +1,36 @@
 #!/usr/bin/python
 
 import os
+import re
 import sys
 import nibabel
 import numpy as np
 import pandas
 from glob import glob
 from nilearn.region import img_to_signals_labels
+from nipy.algorithms.registration.histogram_registration import HistogramRegistration
 from nilearn.image import resample_img
 
-timeseries_file = sys.argv[1]
+vol = sys.argv[1]
 parcels = sys.argv[2]
-output_directory = sys.argv[3]
+output_file = sys.argv[3]
 
 # Read in Peterson ROIs
 parcels = nibabel.load(parcels)
-
-# Read in the full list of the raw timeseries data
-timeseries_file = pandas.read_csv(timeseries_file,sep="\t")
-unique_tasks = timeseries_file.tasks.unique()
-
 parcel_ids = [int(x) for x in np.unique(parcels.get_data()).tolist()]
 parcel_ids.pop(0) # value of 0
 
-# For each unique task, generate list of filtered func data, one for each petersen roi
-for task in unique_tasks:
-  print "STARTING TASK %s" %(task)
-  # We will append each to this data frame
-  df = timeseries_file[timeseries_file.tasks==task]
-  func_data = df.timeseries_files.tolist()
-  func_data_df = pandas.DataFrame()
-  outfile = "%s/%s_timeseries.csv" %(output_directory,task)
-  for vol in func_data:
-    subid = os.path.split(vol)[1].split("_")[0]
-    labels = ["%s_%s" %(subid,x) for x in parcel_ids]
-    print "Processing %s" %(vol)
-    nii_obj = nibabel.load(vol)
-    shape = parcels.get_shape()
-    shape = (shape[0],shape[1],shape[2])
-    nii_resample = resample_img(nii_obj, target_affine=parcels.get_affine(),target_shape=shape,interpolation="continuous")
-    tmp = img_to_signals_labels(nii_resample, parcels, mask_img=None, background_label=0, order='F')
-    df_ss = pandas.DataFrame(tmp[0])
-    df_ss.columns = labels
-    df_ss = df_ss.transpose()    
-    func_data_df = func_data_df.append(df_ss)
-  # Now we have finished all subjects, save tofile
-  func_data_df.to_csv(outfile)
+print "Processing %s" %(vol)
+
+# Use regular expression to find the id, and make labels
+expression = re.compile("/[0-9]+/")
+match = expression.search(vol)
+subid = vol[match.start()+1:match.end()-1]
+labels = ["%s_%s" %(subid,x) for x in parcel_ids]
+nii_obj = nibabel.load(vol)
+nii_resample = resample_img(nii_obj, target_affine=parcels.get_affine(),interpolation="continuous")
+tmp = img_to_signals_labels(nii_resample, parcels, mask_img=None, background_label=0, order='F')
+df_ss = pandas.DataFrame(tmp[0])
+df_ss.columns = labels
+df_ss = df_ss.transpose()    
+df_ss.to_csv(output_file,sep="\t")
--- databases/hcp/run_extract_petersen_timeseries.py
+++ databases/hcp/run_extract_petersen_timeseries.py
@@ -9,31 +9,38 @@ import pandas
 import os
 
 
-# This will give us full paths to each subject folder, where there is filtered func data for each task
-timeseries_file = "/scratch/PI/russpold/work/HCP/group_maps/doc/hcp_timeseries_paths.tsv"
-
 # Output directory
-output_directory = "/scratch/PI/russpold/work/HCP/timeseries"
+output_directory = "/scratch/PI/russpold/work/HCP/timeseries/petersen_timeseries"
 
 # The MNI parcels file, 0 should correspond to no label
 parcels_file = "/scratch/PI/russpold/data/PARCELS/PETERSEN/Parcels_MNI_222.nii"
 
 # Read in the full list of the raw timeseries data
+timeseries_file = "/scratch/PI/russpold/work/HCP/group_maps/doc/hcp_timeseries_paths_filtered.tsv"
 timeseries_file = pandas.read_csv(timeseries_file,sep="\t")
 unique_tasks = timeseries_file.tasks.unique()
-timeseries_file = "/scratch/PI/russpold/work/HCP/group_maps/doc/hcp_timeseries_paths.tsv"
+
+# Filter out images that are not in Results folder - we just want unprocessed
+timeseries_file = timeseries_file[timeseries_file["timeseries_files"].str.contains("Results")]
+unique_tasks = timeseries_file.tasks.unique()
 
 for task in unique_tasks:
-  output_file = "%s/%s_timeseries.csv" %(output_directory,task)
-  if not os.path.exists(output_file):
-    filey = ".job/%s_petersen_ts.job" %(task)
-    filey = open(filey,"w")
-    filey.writelines("#!/bin/bash\n")
-    filey.writelines("#SBATCH --job-name=%s_petersen_ts\n" %(task))
-    filey.writelines("#SBATCH --output=.out/ts_%s.out\n" %(task))
-    filey.writelines("#SBATCH --error=.out/ts_%s.err\n" %(task))
-    filey.writelines("#SBATCH --time=2-00:00\n")
-    filey.writelines("#SBATCH --mem=64000\n")
-    filey.writelines("python /scratch/PI/russpold/work/HCP/timeseries/script/extract_petersen_timeseries.py %s %s %s\n" %(timeseries_file,parcels_file,output_directory))
-    filey.close()
-    os.system("sbatch -p russpold .job/%s_petersen_ts.job" %(task))
+  output_task_directory = "%s/%s" %(output_directory,task)
+  df = timeseries_file[timeseries_file.tasks==task]
+  func_data = df.timeseries_files.tolist()
+  for func in func_data:
+    output_base = os.path.split(func)[1].replace(".nii.gz","")
+    if not os.path.exists(output_task_directory): os.mkdir(output_task_directory)
+    output_file = "%s/%s_petersen.tsv" %(output_task_directory,output_base)
+    if not os.path.exists(output_file):
+      filey = ".job/%s_petersen_ts.job" %(task)
+      filey = open(filey,"w")
+      filey.writelines("#!/bin/bash\n")
+      filey.writelines("#SBATCH --job-name=%s_petersen_ts\n" %(task))
+      filey.writelines("#SBATCH --output=.out/ts_%s.out\n" %(task))
+      filey.writelines("#SBATCH --error=.out/ts_%s.err\n" %(task))
+      filey.writelines("#SBATCH --time=2-00:00\n")
+      filey.writelines("#SBATCH --mem=64000\n")
+      filey.writelines("python /scratch/PI/russpold/work/HCP/timeseries/script/extract_petersen_timeseries.py %s %s %s\n" %(func,parcels_file,output_file))
+      filey.close()
+      os.system("sbatch -p russpold .job/%s_petersen_ts.job" %(task))
